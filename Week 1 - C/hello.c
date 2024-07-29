bfce7fc34378011595ef089ff67e4444d6953461
Sabrina Castro <81202040+cassabr@users.noreply.github.com>
2024-07-29 10:03:53 -0300

    Create hello.c

---
+++ Week 1 - C/hello.c
@@ -0,0 +1,10 @@
+// Learning my first code with C
+
+//#include <cs50.h>
+//#include <stdio.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name? ");
+    print("hello, %s\n", answer);
+}
